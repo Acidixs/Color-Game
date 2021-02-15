d87da11b69c961ebd37ee3d1f294568ef9452457
Aleksander <Aleksanderbo10@gmail.com>
2021-02-15 22:24:45 +0100

    made variables local to function

--- src/graph.py
+++ src/graph.py
@@ -9,10 +9,12 @@ class Graph:
         self.db = Database()
 
     def draw_graph(self):
-        self.rounds = self.db.get_round()
-        self.scores = self.db.get_score() 
+        rounds = self.db.get_round()
+        scores = self.db.get_score()
 
-        plt.scatter(self.rounds, self.scores)
+        print(rounds, scores) 
+
+        plt.scatter(rounds, scores)
         plt.xlabel("rounds")
         plt.ylabel("score")
         plt.show()
